8d853abdc88857ddd52f992f2000d133c2d6485f
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2014-11-25 02:26:48 -0600

    fix sauce integration with protractor

--- test/conf-saucelabs.js
+++ test/conf-saucelabs.js
@@ -1,18 +1,36 @@
 /**
  * http://docs.travis-ci.com/user/ci-environment/
  */
-var repo = process.env.TRAVIS_REPO_SLUG;
-var branch = process.env.TRAVIS_BRANCH;
+var repo = process.env.TRAVIS_REPO_SLUG || 'AndersDJohnson/ng-click-select';
+var branch = process.env.TRAVIS_BRANCH || 'testing';
+var buildNumber = process.env.TRAVIS_BUILD_NUMBER || 0;
 /**
  * http://docs.travis-ci.com/user/encryption-keys/
  */
-var sauceKey = process.env.SAUCELABS_ACCESS_TOKEN;
+var sauceUser = process.env.SAUCE_USERNAME || 'AndersDJohnson';
+var sauceKey = process.env.SAUCE_ACCESS_KEY;
 
+if (! sauceKey) {
+  throw new Error("Must provide SAUCE_ACCESS_KEY environment variable.");
+}
+
+var baseUrl = 'https://rawgit.com/' + repo + '/' + branch + '/';
 var specs = require('./specs');
+
+var sauceName = 'Travis "' + repo + '"' + ' ' + buildNumber + ' (' + branch + ')';
 
 exports.config = {
   specs: specs,
-  baseUrl: 'https://rawgit.com/' + repo + '/' + branch,
-  sauceUser: 'AndersDJohnson',
-  sauceKey: sauceKey
+  baseUrl: baseUrl,
+  sauceUser: sauceUser,
+  sauceKey: sauceKey,
+  /**
+   * https://docs.saucelabs.com/reference/test-configuration/
+   */
+  capabilities: {
+    browserName: 'chrome',
+    build: buildNumber,
+    tags: [repo, repo + ' (' + branch + ')', branch],
+    name: sauceName
+  }
 };
